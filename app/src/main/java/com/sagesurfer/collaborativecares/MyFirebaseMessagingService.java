e10803e2d8a99adc86677c890c66c9ce57d49ab1
Mayur Teraiya <teraiyamayurcontact@gmail.com>
2021-07-07 12:29:19 +0530

    Milestone 2 points

--- app/src/main/java/com/sagesurfer/collaborativecares/MyFirebaseMessagingService.java
+++ app/src/main/java/com/sagesurfer/collaborativecares/MyFirebaseMessagingService.java
@@ -3,9 +3,11 @@ package com.sagesurfer.collaborativecares;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
@@ -13,6 +15,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 
 
 import androidx.annotation.NonNull;
@@ -77,15 +80,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private boolean isCall;
     private boolean isChatScreen, IsFriendListingPage, IsGroupListingPage;
+
     SharedPreferences preferencesCheckCurrentActivity;
     SharedPreferences.Editor editor;
+
+    @Override
+    public ComponentName startService(Intent service) {
+        return super.startService(service);
+    }
+
+    @Override
+    public boolean isRestricted() {
+        return super.isRestricted();
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    @Override
+    public ApplicationInfo getApplicationInfo() {
+        return super.getApplicationInfo();
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+    }
+
 
 
     @Override
     public void onMessageReceived(@NonNull @NotNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+        AppLog.d(TAG, "FROM: " + remoteMessage.getFrom());
         AppLog.d(TAG, "data: " + remoteMessage.getData());
+
 
         preferencesCheckCurrentActivity = getSharedPreferences("preferencesCheckCurrentActivity", MODE_PRIVATE);
         isChatScreen = preferencesCheckCurrentActivity.getBoolean("IsChatScreen", false);
@@ -105,12 +137,53 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Logger.error("Debug", "Firebase Notification payload12 : " + remoteMessage.getData().toString(), getApplicationContext());
             JSONObject json = new JSONObject(remoteMessage.getData());
             AppLog.i(TAG, "onMessageReceived: data " + json.toString());
-            handleDataMessage(remoteMessage);
+            handleDataMessage(data);
 
         } else {
             AppLog.i(TAG, "From notification" + remoteMessage.getFrom());
         }
 
+
+//        try {
+//            count++;
+//            json = new JSONObject(remoteMessage.getData());
+//            messageData = new JSONObject(json.getString("message"));
+//            String title = messageData.getString("receiver");
+//            String receiverType = messageData.getString("receiverType");
+//
+//            Log.e(TAG, "received push notification JSONObject: " + messageData);
+//
+//            BaseMessage baseMessage = CometChatHelper.processMessage(new JSONObject(remoteMessage.getData().get("message")));
+//            if (baseMessage instanceof Call) {
+//                call = (Call) baseMessage;
+//                isCall = true;
+//                //if (!AppInfo.isAppRunning(getApplicationContext(), "com.sagesurfer.collaborativecares")) {
+//                showNotifcation(baseMessage);
+//                //}
+//            } else {
+//
+//                showNotifcation(baseMessage);
+//            }
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//            Log.i(TAG, "onMessageReceived: error " + e.getMessage());
+//        }
+
+    }
+
+    private void handleNotification(String message) {
+        Log.d(TAG, "handleNotification: ");
+        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+            // app is in foreground, broadcast the push message
+            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+            pushNotification.putExtra("message", message);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+            // play notification sound
+            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+            notificationUtils.playNotificationSound();
+        }
     }
 
     private void handleDataMessage(RemoteMessage json) {
@@ -140,6 +213,109 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             JSONObject mainobj = new JSONObject(yourString);
 
             JSONObject data = mainobj.getJSONObject("data");
+
+            AppLog.e(TAG, "Jdata" + data.toString());
+
+            String title = data.optString("title");
+            String message = data.optString("message");
+
+//            String message = "This is demo app";
+            String imageUrl = data.optString("image");
+            String type = data.optString("menu_id");
+            String timestamp = data.optString("timestamp");
+            String groupId = data.optString("groupid");
+            String isModerator = data.optString("ismodrator");
+            String ampm = data.optString("ampm");
+
+            Preferences.save(General.GROUP_ID, groupId);
+            Preferences.save(General.IS_MODERATOR, isModerator);
+            Preferences.save(General.IS_PUSH_NOTIFICATION, true);
+            Preferences.save(General.GOAL_AM_PM, ampm);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            timestamp = sdf.format(new Date());
+
+            //List<String> post1Comments = new ArrayList<>();
+            // Collect comments of a certain post
+            //post1Comments.add(groupId);
+            // Attach comments to post
+            List<String> notificationValues = new ArrayList<String>();
+            notificationValues.add(0, title);
+            notificationValues.add(1, message);
+            notificationValues.add(2, type);
+            notificationValues.add(3, groupId);
+            Config.mapOfPosts.put(timestamp, notificationValues);
+
+//            for (Map.Entry<String, List<String>> entry : Config.mapOfPosts.entrySet()) {
+//                String key = entry.getKey();
+//                //String value = entry.getValue();
+//                System.out.printf("%s -> %s%n", entry.getKey(), entry.getValue().get(0) + "-" + entry.getValue().get(1) + "-" + entry.getValue().get(2) + "-" + entry.getValue().get(3));
+//            }
+
+//            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+//                // app is in foreground, broadcast the push message
+//                Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+//                pushNotification.putExtra(General.MESSAGE, message);
+//                pushNotification.putExtra(General.TIMESTAMP, timestamp);
+//                pushNotification.putExtra(General.TITLE, title);
+//                pushNotification.putExtra(General.TYPE, type);
+//                //pushNotification.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+//
+//                // play notification sound
+//                NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+//                notificationUtils.playNotificationSound();
+//
+//                if (TextUtils.isEmpty(imageUrl)) {
+//                    showNotificationMessage(getApplicationContext(),
+//                            title,
+//                            message,
+//                            timestamp, type, pushNotification);
+//                } else {
+//                    // if image is present, show notification with image
+//                    showNotificationMessageWithBigImage(getApplicationContext(),
+//                            title, message, timestamp, type, pushNotification, imageUrl);
+//                }
+//
+//            } else {
+            // app is in background, show the notification in notification tray
+            Intent resultIntent;
+            //commented on 29/04/19 as need to redirect on team announcement, team task list and team events
+                /*if(Integer.parseInt(Preferences.get(General.GROUP_ID)) != 0) {
+                    resultIntent = new Intent(getApplicationContext(), TeamDetailsActivity.class);
+                } else {
+                    resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+                }*/
+            resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+            resultIntent.putExtra(General.GROUP_ID, Preferences.get(General.GROUP_ID));
+            resultIntent.putExtra(General.MESSAGE, message);
+            resultIntent.putExtra(General.TIMESTAMP, timestamp);
+            resultIntent.putExtra(General.TITLE, title);
+            resultIntent.putExtra(General.TYPE, type);
+            // check for image attachment
+
+            AppLog.i(TAG, "Condition");
+            if (TextUtils.isEmpty(imageUrl)) {
+                AppLog.i(TAG, "Image URL without");
+                showNotificationMessage(getApplicationContext(), title, message, timestamp, type, resultIntent);
+            } else {
+                AppLog.i(TAG, "Image URL with");
+                // if image is present, show notification with image
+                showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, type, resultIntent, imageUrl);
+            }
+            //}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void handleDataMessage(Map<String, String> datamap) {
+        try {
+
+            String dataobj=datamap.get("data");
+
+            JSONObject data = new JSONObject(dataobj);
 
             AppLog.e(TAG, "Jdata" + data.toString());
 
